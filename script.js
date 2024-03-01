06c79006f742d7ba775696ab38d040079275e21b
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-03-01 09:18:06 +0600

    added button functionality

--- script.js
+++ script.js
@@ -8,15 +8,31 @@ const showCategories = (categories) => {
   const categorySection = document.getElementById("category-section");
   categories.forEach((category) => {
     console.log(category);
-    const categoryButtonDiv = document.createElement("div");
-    categoryButtonDiv.innerHTML = `
-    <button
-        class="lg:text-[18px] text-[16px] font-medium lg:py-[5px] py-[5px] lg:px-[20px] px-[15px] rounded text-[#252525b3] bg-[#25252526]">
-        <a href="">${category.category}</a>
-    </button>
-    `;
-    categorySection.appendChild(categoryButtonDiv);
+    const categoryButton = document.createElement("button");
+    categoryButton.classList.add(
+      "lg:text-[18px]",
+      "text-[16px]",
+      "font-medium",
+      "lg:py-[5px]",
+      "py-[5px]",
+      "lg:px-[20px]",
+      "px-[15px]",
+      "rounded",
+      "text-[#252525b3]",
+      "bg-[#25252526]"
+    );
+    categoryButton.innerText = `${category.category}`;
+    categorySection.appendChild(categoryButton);
+
+    // Adding click functionality
+    categoryButton.addEventListener("click", function () {
+      loadCategoryData(`${category.category_id}`);
+    });
   });
+};
+
+const loadCategoryData = (categoryID) => {
+  console.log("clicked", categoryID);
 };
 
 loadCategories();
